a443d4caff72e3469b7aa212e1ed21ac00ee0b3f
Philipp Schindler <e1128993@student.tuwien.ac.at>
2014-09-13 18:47:20 +0200

    added blank EKBServiceOrientDB, dependencies are resolved

---
+++ components/ekb/persistence-orientdb/src/main/java/org/openengsb/framework/ekb/persistence/orientdb/EKBServiceOrientDB.java
@@ -0,0 +1,60 @@
+package org.openengsb.framework.ekb.persistence.orientdb;
+
+import org.openengsb.core.ekb.api.EKBCommit;
+import org.openengsb.core.ekb.api.EKBService;
+import org.openengsb.core.ekb.api.Query;
+import org.openengsb.core.ekb.api.TransformationDescriptor;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by Philipp Schindler on 13.09.2014.
+ */
+public class EKBServiceOrientDB implements EKBService {
+
+    @Override
+    public void commit(EKBCommit ekbCommit) {
+
+    }
+
+    @Override
+    public void commit(EKBCommit ekbCommit, UUID headRevision) {
+
+    }
+
+    @Override
+    public void addTransformation(TransformationDescriptor descriptor) {
+
+    }
+
+    @Override
+    public <T> List<T> query(Query query) {
+        return null;
+    }
+
+    @Override
+    public Object nativeQuery(Object query) {
+        return null;
+    }
+
+    @Override
+    public UUID getLastRevisionId() {
+        return null;
+    }
+
+    @Override
+    public void deleteCommit(UUID headRevision) {
+
+    }
+
+    @Override
+    public EKBCommit loadCommit(UUID revision) {
+        return null;
+    }
+
+    @Override
+    public <T> T getModel(Class<T> model, String oid) {
+        return null;
+    }
+}
