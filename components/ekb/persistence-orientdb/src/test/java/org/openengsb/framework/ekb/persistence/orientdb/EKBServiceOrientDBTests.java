7289dc641886258d8238ce565c00d9975aa6381c
Philipp Schindler <e1128993@student.tuwien.ac.at>
2014-09-29 13:19:07 +0200

    Added support for OrientDB in embedded mode (persistent), and OrientDB in in-memory mode; added some basic benchmarks

--- components/ekb/persistence-orientdb/src/test/java/org/openengsb/framework/ekb/persistence/orientdb/EKBServiceOrientDBTests.java
+++ components/ekb/persistence-orientdb/src/test/java/org/openengsb/framework/ekb/persistence/orientdb/EKBServiceOrientDBTests.java
@@ -1,3 +1,20 @@
+/**
+ * Licensed to the Austrian Association for Software Tool Integration (AASTI)
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. The AASTI licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.openengsb.framework.ekb.persistence.orientdb;
 
 import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
@@ -24,10 +41,6 @@ import java.math.BigDecimal;
 import java.util.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-
-/**
- * Created by Philipp Schindler on 14.09.2014.
- */
 
 @RunWith(MockitoJUnitRunner.class)
 public class EKBServiceOrientDBTests {
--- components/ekb/persistence-orientdb/src/test/java/org/openengsb/framework/ekb/persistence/orientdb/visualization/DotExporter.java
+++ components/ekb/persistence-orientdb/src/test/java/org/openengsb/framework/ekb/persistence/orientdb/visualization/DotExporter.java
@@ -1,3 +1,20 @@
+/**
+ * Licensed to the Austrian Association for Software Tool Integration (AASTI)
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. The AASTI licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.openengsb.framework.ekb.persistence.orientdb.visualization;
 
 import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
@@ -8,9 +25,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
-/**
- * Created by Philipp Schindler on 21.09.2014.
- */
 public class DotExporter {
 
     public static void export(ODatabaseDocumentTx database, String filepath, String ...classes) {
